e229b98fbd20454c147ae5644269fe657d1cafe5
Andrii <andriibozhko1@gmail.com>
2019-03-05 21:17:46 +0200

    add styles to albums list

--- src/components/AlbumsList/AlbumsList.js
+++ src/components/AlbumsList/AlbumsList.js
@@ -7,10 +7,13 @@ export default class AlbumsList extends Component {
       <div className="AlbumsList">
         <ul className="AlbumsList__list">
           {this.props.albums.map(album => (
-            <li className="AlbumsList__item" key={album.id}>
-              <div className="AlbumsList__album-card">
-                <div className="AlbumList__album-card-tittle">{ album.title }</div>
-              </div>
+            <li className="AlbumsList__item" key={album.id}>                                     
+              <div className="AlbumsList__album-card">              
+                <div className="AlbumsList__content">
+                  <div className="AlbumsList__id">Album #: { album.id }</div><br />
+                  <div className="AlbumsList__album-card-tittle"> { album.title }</div>
+                </div>
+              </div>                            
             </li>
           ))}
         </ul>
