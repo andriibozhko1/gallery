825b4b39819a5a6746ac8de01c26f3f6c7b9e468
Andrii <andriibozhko1@gmail.com>
2019-03-06 11:18:36 +0200

    to gh-pages

---
+++ precache-manifest.fdd71f1bd1cd8393424ed53a9484effe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5090e61580d66db16620",
+    "url": "/gallery/static/css/main.b736c070.chunk.css"
+  },
+  {
+    "revision": "5090e61580d66db16620",
+    "url": "/gallery/static/js/main.5090e615.chunk.js"
+  },
+  {
+    "revision": "4d7bafb501d7e1c3e8b6",
+    "url": "/gallery/static/js/runtime~main.4d7bafb5.js"
+  },
+  {
+    "revision": "277705b8771a08004f62",
+    "url": "/gallery/static/css/2.af097e3c.chunk.css"
+  },
+  {
+    "revision": "277705b8771a08004f62",
+    "url": "/gallery/static/js/2.277705b8.chunk.js"
+  },
+  {
+    "revision": "833963ca5b5ec0ae624c84ede597a61d",
+    "url": "/gallery/index.html"
+  }
+];
